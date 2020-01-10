951d2892be84ecfcce6d19e1164382dd8cc1bfd5
AlvCarballo <alv.carballo@outlook.es>
2020-01-10 09:00:57 +0100

    first commit

---
+++ src/es/Studium/cadEjerExtra/MostrarCaract.java
@@ -0,0 +1,16 @@
+package es.Studium.cadEjerExtra;
+
+public class MostrarCaract {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int i;
+		String cadena="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		for (i=0;i<cadena.length();i++)
+		{
+			System.out.println(cadena.charAt(i));
+		}
+	}
+
+}
